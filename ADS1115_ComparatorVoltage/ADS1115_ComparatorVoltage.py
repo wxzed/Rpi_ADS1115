5537a1356e213b2071cde2addc6e9062600af027
wxzed <xiao.wu@dfrobot.com>
2018-01-29 09:56:16 +0800

    first commit

---
+++ ADS1115_ComparatorVoltage/ADS1115_ComparatorVoltage.py
@@ -0,0 +1,23 @@
+import sys
+sys.path.append('../')
+import time
+ADS1115_REG_CONFIG_PGA_6_144V		= 0x00 # 6.144V range = Gain 2/3
+ADS1115_REG_CONFIG_PGA_4_096V		= 0x02 # 4.096V range = Gain 1
+ADS1115_REG_CONFIG_PGA_2_048V		= 0x04 # 2.048V range = Gain 2 (default)
+ADS1115_REG_CONFIG_PGA_1_024V		= 0x06 # 1.024V range = Gain 4
+ADS1115_REG_CONFIG_PGA_0_512V		= 0x08 # 0.512V range = Gain 8
+ADS1115_REG_CONFIG_PGA_0_256V		= 0x0A # 0.256V range = Gain 16
+
+from DFRobot_ADS1115 import ADS1115
+ads1115 = ADS1115()
+
+while True :
+	ads1115.setAddr_ADS1115(0x48)
+	ads1115.set_gain(ADS1115_REG_CONFIG_PGA_4_096V)
+	ads1115.set_channel()
+	ads1115.config_differential()
+	time.sleep(0.1)
+	adc = ads1115.read_adc()
+	print "Digital Value of Analog Input : %d "%(adc['r'])
+	print " ********************************************* "
+	time.sleep(0.8)
